72609901a79bea819d33b8179c5687bdeab95b9f
Dimitrije Stevanovic <dimitrije12012007@gmail.com>
2024-06-28 12:57:14 +0200

    Add project files.

---
+++ Zakazivanje/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Zakazivanje
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Zakazivanje/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Zakazivanje
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
